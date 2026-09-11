9a17c4efebd38b7fc0f2f808f488392aa4a6e532
amirkx-rz <amianamizi@gmail.com>
2026-09-11 23:32:05 +0330

    Update core.lua

--- core.lua
+++ core.lua
@@ -1,6 +1,6 @@
 script_name("Private Assistant - Cloud Core")
 script_author("Diagnostic")
-script_version("FINAL_SYNCED")
+script_version("FINAL_FIXED_CLEAN")
 
 local sampev = require 'samp.events'
 local inicfg = require 'inicfg'
@@ -20,23 +20,17 @@ local currentPoleCoords = nil
 local completedJobs = 0
 local sessionMoney = config.stats.savedMoney or 0
 local totalPoles = config.stats.savedPoles or 0
-local isTeleporting = false
+local hasTeleported = false
 
--- لینک گوگل فرم شما
 local GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSckd95-_wZKdXN9p0N-AS5c5wj_8H0pf1JZ4wAPrhVxOvSx6Q/formResponse?entry.401900459=%s&entry.713901036=%s&entry.1717034231=%d&entry.219007459=%d"
-
-local font = nil
-local activeSpectators = {}
 
 function main()
     while not isSampAvailable() do wait(100) end
     while not sampIsLocalPlayerSpawned() do wait(200) end
 
-    font = renderCreateFont("Arial", 11, 5)
-
     sampRegisterChatCommand("bot", function()
         autoPilot = not autoPilot
-        sampAddChatMessage(autoPilot and "{00FF00}Bot ROSHAN" or "{FF0000}Bot KHAMOSH", -1)
+        sampAddChatMessage(autoPilot and "{00FF00}[Bot] ROSHAN" or "{FF0000}[Bot] KHAMOSH", -1)
         if autoPilot then
             startJobCycle()
         else
@@ -44,69 +38,20 @@ function main()
         end
     end)
 
-    sampRegisterChatCommand("atp", function()
-        local ok, x, y, z = getTargetBlipCoordinates()
-        if ok then
-            executeSyncedTeleport(x, y, z)
-        else
-            sampAddChatMessage("{FF0000}[Error] Marker bezanid!", -1)
-        end
-    end)
+    sampAddChatMessage("{00FF00}[Private Core] {FFFFFF}Loaded! Cmd: {00FFFF}/bot", -1)
 
-    sampAddChatMessage("{00FF00}[Private Core] {FFFFFF}Loaded! Cmd: {00FFFF}/bot {FFFFFF}| {00FFFF}/atp", -1)
-
-    -- رندر اسپکت و استف آنلاین (بدون هیچ ردی از آرت)
-    lua_thread.create(function()
-        while true do
-            wait(0)
-            if font and sampIsLocalPlayerSpawned() then
-                local sw, sh = getScreenResolution()
-                
-                -- اسپکت فایندر (سمت چپ)
-                local specY = sh / 2
-                renderFontDrawText(font, "--- Spectators ---", 10, specY - 18, 0xFF00FFFF)
-                local hasSpec = false
-                for name, time in pairs(activeSpectators) do
-                    if os.clock() - time < 3.0 then 
-                        renderFontDrawText(font, ">> " .. name, 10, specY, 0xFFFF0000)
-                        specY = specY + 16
-                        hasSpec = true
-                    else
-                        activeSpectators[name] = nil
-                    end
-                end
-                if not hasSpec then renderFontDrawText(font, "None", 10, specY, 0xFF00FF00) end
-                
-                -- ادمین و هلپر (سمت راست)
-                local yOffset = sh / 3
-                renderFontDrawText(font, "--- Staff Online ---", sw - 170, yOffset, 0xFFFFAA00)
-                yOffset = yOffset + 18
-                local foundStaff = false
-                for i = 0, sampGetMaxPlayerId(true) do
-                    if sampIsPlayerConnected(i) then
-                        local name = sampGetPlayerNickname(i)
-                        if name and type(name) == "string" then
-                            if name:find("%[A%]") or name:find("%[H%]") or name:find("Admin") then
-                                renderFontDrawText(font, name .. " ["..i.."]", sw - 170, yOffset, 0xFFFF0000)
-                                yOffset = yOffset + 16
-                                foundStaff = true
-                            end
-                        end
-                    end
-                end
-                if not foundStaff then renderFontDrawText(font, "Safe", sw - 170, yOffset, 0xFF00FF00) end
-            end
-        end
-    end)
-
-    -- حلقه دکل‌ها
+    -- ترِد نظارت ربات
     lua_thread.create(function()
         while true do
             wait(250)
-            if autoPilot and currentPoleCoords and not isTeleporting then
+            if autoPilot and currentPoleCoords and not hasTeleported then
                 local mx, my, mz = getCharCoordinates(PLAYER_PED)
                 if getDistanceBetweenCoords3d(mx, my, mz, currentPoleCoords.x, currentPoleCoords.y, currentPoleCoords.z) > 3.0 then
-                    executeSyncedTeleport(currentPoleCoords.x, currentPoleCoords.y, currentPoleCoords.z)
+                    -- ارسال دستور فابریک تلپورت به آرت
+                    sampProcessChatInput("/atp")
+                    hasTeleported = true
+                else
+                    hasTeleported = true
                 end
             end
         end
@@ -120,45 +65,22 @@ function startJobCycle()
     lua_thread.create(function() wait(500); sampSendChat("/pl") end)
 end
 
--- اتصال مستقیم به موتور سینک آرت در حافظه
-function executeSyncedTeleport(tx, ty, tz)
-    isTeleporting = true
-    
-    -- روشن کردن متغیر محافظتی آرت در فضای مشترک
-    rawset(_G, "TeleportSync", true)
-    
-    lua_thread.create(function()
-        local targetZ = (tz and tz > 0.0) and tz or 15.0
-        setCharCoordinates(PLAYER_PED, tx, ty, targetZ)
-        wait(2000)
-        rawset(_G, "TeleportSync", false)
-        isTeleporting = false
-    end)
+function sampev.onSetCheckpoint(pos, rad) 
+    if autoPilot then 
+        currentPoleCoords = pos 
+        hasTeleported = false 
+    end 
 end
 
-function sampev.onSetCheckpoint(pos, rad) if autoPilot then currentPoleCoords = pos end end
-function sampev.onSetRaceCheckpoint(t, pos, np, r) if autoPilot then currentPoleCoords = pos end end
-function sampev.onDisableCheckpoint() currentPoleCoords = nil end
-function sampev.onDisableRaceCheckpoint() currentPoleCoords = nil end
-
-function sampev.onPlayerSync(playerId, data)
-    if not sampIsLocalPlayerSpawned() then return end
-    pcall(function()
-        if sampIsPlayerConnected(playerId) then
-            local mx, my, mz = getCharCoordinates(PLAYER_PED)
-            local dist = getDistanceBetweenCoords3d(mx, my, mz, data.position.x, data.position.y, data.position.z)
-            if dist < 2.0 then
-                local hasPed, pedHandle = sampGetCharHandleBySampPlayerId(playerId)
-                if not hasPed or (hasPed and doesCharExist(pedHandle) and not isCharOnScreen(pedHandle)) then
-                    local specName = sampGetPlayerNickname(playerId)
-                    if specName and specName ~= "" then
-                        activeSpectators[specName] = os.clock()
-                    end
-                end
-            end
-        end
-    end)
+function sampev.onSetRaceCheckpoint(t, pos, np, r) 
+    if autoPilot then 
+        currentPoleCoords = pos 
+        hasTeleported = false 
+    end 
 end
+
+function sampev.onDisableCheckpoint() currentPoleCoords = nil; hasTeleported = false end
+function sampev.onDisableRaceCheckpoint() currentPoleCoords = nil; hasTeleported = false end
 
 function sendStatsToGoogle(modeName)
     if totalPoles > 0 and GOOGLE_FORM_URL ~= "" then
@@ -199,6 +121,7 @@ function sampev.onServerMessage(color, text)
         pcall(inicfg.save, config, iniFile)
 
         currentPoleCoords = nil
+        hasTeleported = false
         sampAddChatMessage("[Bot] Dakal sabt shod! (" .. completedJobs .. "/4)", 0x00FF00)
 
         if completedJobs >= 4 then
@@ -214,6 +137,7 @@ function sampev.onServerMessage(color, text)
     if text:find("یافت نشد") or text:find("هیچ") then
         config.settings.jobMode = (config.settings.jobMode == "repair") and "rob" or "repair"
         completedJobs = 0
+        hasTeleported = false
         lua_thread.create(function() wait(2000); startJobCycle() end)
     end
 end
